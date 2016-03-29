3b424a98da6eb282ba60752331edc925d3c4d2d8
John Detlefs <jdetle@gmail.com>
2016-03-29 14:22:33 -0700

    first javascript

---
+++ javascripts/hello.js
@@ -0,0 +1,8 @@
+var wrapper = document.getElementById('wrapper');
+document.addEventListener('scroll', function(){
+  console.log('scroll');
+  return false;}, false);
+
+function checkForSecretCode(names) {
+    return false;
+}
